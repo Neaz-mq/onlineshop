9c7894c1ccb80d23eaa023b759a1a1ba1239934b
Neaz-mq <neazmorshed666@gmail.com>
2022-12-30 18:43:32 +0600

    5th

---
+++ resources/views/backend/partials/layouts/home.blade.php
@@ -0,0 +1,5 @@
+@extends('backend.master')
+
+@section('content')
+<h1>My home</h1>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('backend.master');
+
+
+Route::get('/', function(){
+
+    return view('backend.partials.layouts.home');
+});
+
+Route::get('/contact', function(){
+
+    return view('backend.partials.layouts.contact');
 });
